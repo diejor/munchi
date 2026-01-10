74db59a82430e0bd3dd121c2cec85d849d19229b
Diego Alejandro Rodrigues Rodriguez <diejorarr@gmail.com>
2026-01-10 12:13:52 -0600

    Dualgrid with mountains

--- scenes/player/animation_tree.gd
+++ scenes/player/animation_tree.gd
@@ -3,7 +3,7 @@ extends AnimationTree
 
 var is_moving: bool = false
 
-func on_update(input: Vector2) -> void:
+func on_input(input: Vector2) -> void:
 	if not input.is_zero_approx():
 		set("parameters/Idle/blend_position", input)
 		set("parameters/Walk/blend_position", input)
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -5,9 +5,8 @@ extends CharacterBody2D
 @onready var player_art: Node2D = %PlayerArt
 @onready var art_animator: ArtAnimator = player_art.get_node("%AnimationTree")
 
-
 func _physics_process(_delta: float) -> void:
 	var input: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = input * speed
-	art_animator.on_update(input)
+	art_animator.on_input(input)
 	move_and_slide()
