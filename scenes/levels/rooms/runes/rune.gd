a86b18a608bf9a9bfd006225dbd50f30f894e6a2
Diego Rodrigues <diejorarr@gmail.com>
2026-01-17 17:00:29 -0600

    Changes

--- scenes/levels/rooms/runes/rune.gd
+++ scenes/levels/rooms/runes/rune.gd
@@ -1,8 +1,6 @@
 class_name RuneBase
 extends Node2D
 
-@export var enemies_ary: Array[CharacterNPC]
-
 @warning_ignore("unused_signal")
 signal activate(rune: RuneBase)
 
--- scenes/player/player_manager.gd
+++ scenes/player/player_manager.gd
@@ -1,4 +1,8 @@
 extends Node
 
 var player: CharacterPlayer:
-	get: return get_tree().get_nodes_in_group("player")[0]
+	get:
+		var p = get_tree().get_nodes_in_group("player")
+		if not p.is_empty():
+			return p[0]
+		return null
